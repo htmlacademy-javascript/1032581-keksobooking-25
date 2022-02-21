bd7e710f4a876c7e76e405aa6ff03b0d78c80e4c
Keks <keks@htmlacademy.ru>
2022-02-21 21:45:07 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,17 @@
+const getRandomInteger = (min, max) => {
+  if(min < 0 || max < 0 || max < min || min === max) {
+    return false;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+getRandomInteger(1, 3);
+
+const getRandomFloat = (min, max, numberOfSymbolsAfterComma) => {
+  if(min < 0 || max < 0 || max < min || min === max) {
+    return false;
+  }
+  return (Math.random() * (max - min) + min).toFixed(numberOfSymbolsAfterComma);
+};
+
+getRandomFloat(1.1, 1.2, 1);
