04a5e734f26a753fc9109b236a42f10a5c684fff
danylo <logolevel@gmail.com>
2022-03-20 20:59:00 +0200

    Fix counter for avatar path; set variables for locations; add randomness for getRandomArrayOfStrings function

--- js/main.js
+++ js/main.js
@@ -61,30 +61,48 @@ const getRandomFloat = (min, max, numberOfSymbolsAfterComma) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const getRandomArrayOfStrings = (elements) => elements.slice(0, getRandomInteger(1, elements.length - 1));
+const getRandomArrayOfStrings = (elements, randomNumber) => {
+  const randomArray = [];
 
-const createAdvertisement = (element, index) => ({
-  author: {
-    avatar: `img/avatars/user${index + 1}.png`,
-  },
-  offer: {
-    title: getRandomArrayElement(titles),
-    address: `${getRandomFloat(35.65000, 35.70000, 5)}, ${getRandomFloat(139.70000, 139.80000, 5)}`,
-    price: getRandomInteger(1500, 6000),
-    type: getRandomArrayElement(types),
-    rooms: getRandomInteger(1, 4),
-    guests: getRandomInteger(1, 12),
-    checkin: getRandomArrayElement(times),
-    checkout: getRandomArrayElement(times),
-    features: getRandomArrayOfStrings(features),
-    description: getRandomArrayElement(descriptions),
-    photos: getRandomArrayOfStrings(photos),
-  },
-  location: {
-    lat: getRandomFloat(35.65000, 35.70000, 5),
-    lng: getRandomFloat(139.70000, 139.80000, 5),
-  },
-});
+  for(let i = 0; i < randomNumber; i++){
+    randomArray.push(elements[getRandomInteger(0, elements.length - 1)]);
+  }
+
+  const uniqueArray = new Set(randomArray);
+  Array.from(uniqueArray);
+
+  return uniqueArray;
+};
+
+const getArrayOfStrings = (elements) => elements.slice(0, getRandomInteger(1, elements.length - 1));
+
+const createAdvertisement = (element, index) => {
+  const LOCATION_LAT = getRandomFloat(35.65000, 35.70000, 5);
+  const LOCATION_LNG = getRandomFloat(139.70000, 139.80000, 5);
+
+  return ({
+    author: {
+      avatar: `img/avatars/user${(index + 1 < 10) ? `0${index + 1}` : (index + 1)}.png`,
+    },
+    offer: {
+      title: getRandomArrayElement(titles),
+      address: `${LOCATION_LAT}, ${LOCATION_LNG}`,
+      price: getRandomInteger(1, Number.MAX_SAFE_INTEGER),
+      type: getRandomArrayElement(types),
+      rooms: getRandomInteger(1, Number.MAX_SAFE_INTEGER),
+      guests: getRandomInteger(1, Number.MAX_SAFE_INTEGER),
+      checkin: getRandomArrayElement(times),
+      checkout: getRandomArrayElement(times),
+      features: getRandomArrayOfStrings(features, getRandomInteger(0, features.length - 1)),
+      description: getRandomArrayElement(descriptions),
+      photos: getArrayOfStrings(photos),
+    },
+    location: {
+      lat: LOCATION_LAT,
+      lng: LOCATION_LNG,
+    },
+  });
+};
 
 const similarAdvertisements = Array.from({length: 10}, createAdvertisement);
 
