07646b8d30856bcf93bad44b5c5f61595cce53d7
danylo <logolevel@gmail.com>
2022-04-02 06:22:01 +0300

    Add function for disable and active interactive form elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 import {generateAdvertisingMarkup} from './modules/generate-advertising-markup.js';
+import {toInactiveState, toActiveState} from './modules/form.js';
 
+toInactiveState();
+toActiveState();
 generateAdvertisingMarkup();
