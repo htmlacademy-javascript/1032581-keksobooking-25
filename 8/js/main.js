0ab40b90ef92ac0128329c7f69bd32c2768b94e9
Keks <keks@htmlacademy.ru>
2022-04-04 00:25:59 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import {generateAdvertisingMarkup} from './modules/generate-advertising-markup.js';
+import {toInactiveState, toActiveState} from './modules/form.js';
+
+toInactiveState();
+toActiveState();
+
+generateAdvertisingMarkup();
