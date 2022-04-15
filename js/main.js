ca4e619281bfac68e4cd71e9511dc2e975c392b2
danylo <logolevel@gmail.com>
2022-04-15 15:18:51 +0300

    Add debounce function for the filter data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import {deactivateForm} from './modules/form.js';
 import {deactivateFilter} from './modules/filter.js';
 import {initMap} from './modules/map.js';
-import './modules/image-preview.js';
 
 deactivateForm();
 deactivateFilter();
--- js/modules/util.js
+++ js/modules/util.js
@@ -20,4 +20,13 @@ const showAlert = (message) => {
   document.body.append(alertContainer);
 };
 
-export {showAlert, arrayIsEmpty};
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export {showAlert, arrayIsEmpty, debounce};
